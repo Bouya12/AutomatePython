d077be0e5bb14f11c1319a0cde869c1a6d99ef9d
bouya12 <tsuredurebouya@gmail.com>
2019-06-09 23:43:04 +0900

    fix initial comment

--- inventory.py
+++ inventory.py
@@ -1,7 +1,7 @@
-#program     : make_grid.py
-#chapterNo   : 4.10.2
-#description : 引数としてxy座標で文字で絵を描くリストを渡し、関数で描画する
-#makeDate    : 2019/06/04
+#program     : inventory.py
+#chapterNo   : 5.6.1
+#description : 持ち物リストを引数として、各アイテムの数と全アイテムの総数を表示する
+#makeDate    : 2019/06/09
 
 def display_inventory(inventory):
     print('持ち物リスト:')
